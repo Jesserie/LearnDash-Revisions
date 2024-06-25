edda21e32d695c293721eb185a5278ffde67610e
Jesserie <46533524+Jesserie@users.noreply.github.com>
2024-06-26 02:37:27 +0800

    learndash-revisions.php

--- learndash-revisions/learndash-revisions.php
+++ learndash-revisions/learndash-revisions.php
@@ -14,3 +14,11 @@ function enable_revisions_for_learndash() {
     add_post_type_support('sfwd-assignment', 'revisions');
 }
 add_action('init', 'enable_revisions_for_learndash');
+
+function learndash_revision_limit($num, $post) {
+    if (in_array($post->post_type, array('sfwd-courses', 'sfwd-lessons', 'sfwd-topic', 'sfwd-quiz', 'sfwd-assignment'))) {
+        $num = 5; // Set the number of revisions you want to keep
+    }
+    return $num;
+}
+add_filter('wp_revisions_to_keep', 'learndash_revision_limit', 10, 2);
