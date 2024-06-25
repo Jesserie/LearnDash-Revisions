f7d45a70f4da2df97c9e54477f57c3b61b962d7d
Jesserie <46533524+Jesserie@users.noreply.github.com>
2024-06-26 02:54:26 +0800

    learndash-revisions.php

--- learndash-revisions.php
+++ learndash-revisions.php
@@ -1,10 +1,4 @@
 <?php
-/*
-Plugin Name: LearnDash Revisions
-Description: Enable revisions for LearnDash course post types.
-Version: 1.1
-Author: Jesse Rie Santamina
-*/
 
 function enable_revisions_for_learndash() {
     add_post_type_support('sfwd-courses', 'revisions');
